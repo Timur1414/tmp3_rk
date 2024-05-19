8506bad93079b0a353d89d92793c1d581cf17e47
Ekate <ekate@Ubuntu22.11.myguest.virtualbox.org>
2024-05-19 23:37:55 +0300

    new tests

--- tests/Singleton_test.cpp
+++ tests/Singleton_test.cpp
@@ -1,43 +1,54 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <thread>
+#include <vector>
+#include "../singleton.cpp"
 
-class Singleton {
-public:
-    static Singleton* getInstance() {
-        if (_instance == 0) {
-            _instance = new Singleton();
-        }
-        return _instance;
-    }
 
-private:
-    static Singleton* _instance;
-    Singleton() {
-        std::cout << "singleton..." << std::endl;
-    }
-};
+Singleton* Singleton::_instance = nullptr;
 
-Singleton* Singleton::_instance = 0;
-
-TEST(SingletonTest, GetInstance) {
+TEST(SingletonTest, SingleInstanceWithoutThreads) {
     Singleton* sgn1 = Singleton::getInstance();
     Singleton* sgn2 = Singleton::getInstance();
     ASSERT_EQ(sgn1, sgn2);
 }
 
-TEST(SingletonTest, DeleteInstance) {
+TEST(SingletonTest, ThreadSafety) {
+    std::vector<std::thread> threads;
+    for (int i = 0; i < 10; ++i) {
+        threads.emplace_back([]() {
+            Singleton::getInstance();
+        });
+    }
+
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
     Singleton* sgn1 = Singleton::getInstance();
     Singleton* sgn2 = Singleton::getInstance();
-    delete sgn1;
-    ASSERT_NO_THROW(Singleton::getInstance());
+    ASSERT_EQ(sgn1, sgn2);
 }
 
-TEST(SingletonTest, MultipleInstances) {
+TEST(SingletonTest, MultipleInstancesWithoutThreadSafety) {
     Singleton* sgn1 = Singleton::getInstance();
-    Singleton* sgn2 = Singleton::getInstance();
-    Singleton* sgn3 = Singleton::getInstance();
-    ASSERT_EQ(sgn1, sgn2);
-    ASSERT_EQ(sgn2, sgn3);
+    Singleton* sgn2 = nullptr;
+    Singleton* sgn3 = nullptr;
+
+    std::thread t1([&]() {
+        sgn2 = Singleton::getInstance();
+    });
+
+    std::thread t2([&]() {
+        sgn3 = Singleton::getInstance();
+    });
+
+    t1.join();
+    t2.join();
+
+    ASSERT_NE(sgn1, sgn2);
+    ASSERT_NE(sgn1, sgn3);
+    ASSERT_NE(sgn2, sgn3);
 }
 
 int main(int argc, char** argv) {
